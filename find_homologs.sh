df7dee97ed72159531ca5694d9f99d8f115d26e5
davemenezes <davekeegan23@gmail.com>
2025-09-08 20:45:41 -0400

    Update find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/bash
-
 query="$1"
 subject="$2"
 output_file="$3"
-
 tblastn -query "$query" -subject "$subject" -outfmt '6 qseqid sseqid pident length' \
  | awk - v qlen=$(grep -v ">" "query" | ec -c) '$3>30 && $4/qlen>0.9' \
  > "$output_file"
-
 echo $(wc -1 < "$output_file")
