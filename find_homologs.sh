60b0e3b5aacb7aa48d5c2cd1a69541a1850c79cf
davemenezes <davekeegan23@gmail.com>
2025-09-08 21:32:57 -0400

    Add files via upload

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/bash
+#!/bin/bash
 query="$1"
 subject="$2"
 output_file="$3"
-tblastn -query "$query" -subject "$subject" -outfmt '6 qseqid sseqid pident length' \
- | awk - v qlen=$(grep -v ">" "query" | ec -c) '$3>30 && $4/qlen>0.9' \
- > "$output_file"
-echo $(wc -1 < "$output_file")
+
+qlen=$(grep -v "^>" "$query" | tr -d '\n' | wc -c)
+
+tblastn -query "$query" -subject "$subject" -outfmt "6 qseqid sseqid pident length" \
+    | awk -v qlen="$qlen" '$3 > 30 && $4 >= 0.9 * qlen' > "$output_file"
+
+wc -l < "$output_file"
